ba44f23ee57e2ba69dc2eed51bf73d7403a14c64
ryan leslie <ryanleslie33@aol.com>
2018-10-19 13:25:36 -0700

    try to add more content

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,33 @@ $(document).ready(function(){
     var userage = parseInt($("#age").val());
     var usergender = $("#gender").val();
     var userskill = parseInt($("#skill").val());
+
     if ((usergender === 'male') && (userage <= 30) && (userskill <= 2)){
       $("#ruby").show()
     }else if (userskill >= 2){
       $("#c-sharp").show()
-      }
-      if ((userage >= 40) && (userskill <= 1)){
-        $("#html").show()
-      }else if ((userage <= 50) && (userskill > 1) && (usergender === 'female')){
-        $("#java").show()
-        $("#c-sharp").hide()
+    }
+    if ((userage >= 40) && (userskill <= 1)){
+      $("#html").show()
+    }else if ((userage <= 50) && (userskill > 1) && (usergender === 'female')){
+      $("#java").show()
+      $("#c-sharp").hide()
 
-}
+    }
 
-    });
+
   });
+});
+$(document).ready(function(){
+  $(".counsler").submit(function(event){
+    event.preventDefault();
+    var userName = $("#name").val();
+    var userdate = $("#dates").val();
+
+
+    $("#outputAppointment").show();
+    $("#userName").text(userName);
+    $("#appointmentDate").text(userdate);
+
+});
+});
