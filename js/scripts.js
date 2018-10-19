ae4ab4aa54adb5e998a4f6a79e7c8a440eb636d5
ryan leslie <ryanleslie33@aol.com>
2018-10-19 16:18:42 -0700

    try to get the calender dates to work

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
     var userskill = parseInt($("#skill").val());
     var motivation = parseInt($("#motive").val());
     var school = parseInt($("#school").val());
-    if ((usergender === 'male') && (userage <= 30) && (userskill <= 4 )) {
+    if ((usergender === 'male' || 'female') && (userage <= 30) && (userskill <= 4 )) {
       $("#ruby").show()
     }else if (userskill >= 2){
       $("#c-sharp").show()
       $("#c-sharp").html(" c-sharp <img src='img/cc.png' alt='code language'>");
     }
-    if ((userage >= 40) && (userskill <= 1)) || (motivation <= 1){
+    if ((userage >= 40) && (userskill <= 1) || (motivation <= 1)){
       $("#html").show()
       $("#html").html(" html <img src='img/html.png' alt='code language'>");
     }else if ((userage <= 50) && (userskill > 1) && (usergender === 'female')){
@@ -27,19 +27,19 @@ $("#java").html(" javascript <img src='img/java.jpg' alt='asdfasdf'>");
         $("#ruby").hide()
     }
 
-
   });
 });
 $(document).ready(function(){
-  $(".counsler").submit(function(event){
+  $("#john").submit(function(event){
     event.preventDefault();
     var userName = $("#name").val();
     var userdate = $("#dates").val();
-
-
-    $("#outputAppointment").show();
-    $("#userName").text(userName);
-    $("#appointmentDate").text(userdate);
+$("#answers").show();
+$("#").text(userName);
+$("#dates").text(userdate);
 
 });
 });
+
+    // $("#userName").text(userName);
+    // $("#appointmentDate").text(userdate);
