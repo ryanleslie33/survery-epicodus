3b9ddb2b3d070e0dda46fbca5a2e08a445ab2495
ryan leslie <ryanleslie33@aol.com>
2018-10-19 14:59:47 -0700

    add new variables and if statements into js

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,26 @@ $(document).ready(function(){
     var userage = parseInt($("#age").val());
     var usergender = $("#gender").val();
     var userskill = parseInt($("#skill").val());
-
-    if ((usergender === 'male') && (userage <= 30) && (userskill <= 2)){
+    var motivation = parseInt($("#motive").val());
+    var school = parseInt($("#school").val());
+    if ((usergender === 'male') && (userage <= 30) && (userskill <= 4 )) {
       $("#ruby").show()
     }else if (userskill >= 2){
       $("#c-sharp").show()
       $("#c-sharp").html(" c-sharp <img src='img/cc.png' alt='code language'>");
     }
-    if ((userage >= 40) && (userskill <= 1)){
+    if ((userage >= 40) && (userskill <= 1)) || (motivation <= 1){
       $("#html").show()
       $("#html").html(" html <img src='img/html.png' alt='code language'>");
     }else if ((userage <= 50) && (userskill > 1) && (usergender === 'female')){
       $("#java").show()
       $("#c-sharp").hide()
 $("#java").html(" javascript <img src='img/java.jpg' alt='asdfasdf'>");
+    }
+    if ((school >= 4) && (userskill >= 4)){
+        $("#c-sharp").html(" c-sharp <img src='img/cc.png' alt='code language'>");
+        $("#c-sharp").show()
+        $("#ruby").hide()
     }
 
 
