af96771d39f844cdf79b2604ac7eb3e5111ebeea
ryan leslie <ryanleslie33@aol.com>
2018-10-19 11:24:39 -0700

    add new else if functions into js

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,18 @@ $(document).ready(function(){
     var userage = parseInt($("#age").val());
     var usergender = $("#gender").val();
     var userskill = parseInt($("#skill").val());
-    if ((usergender === 'male') && (userage === 30)){
+    if ((usergender === 'male') && (userage <= 30) && (userskill <= 2)){
+      $("#ruby").show()
+    }else if (userskill >= 2){
+      $("#c-sharp").show()
+      }
+      if ((userage >= 40) && (userskill <= 1)){
+        $("#html").show()
+      }else if ((userage <= 50) && (userskill > 1) && (usergender === 'female')){
+        $("#java").show()
+        $("#c-sharp").hide()
 
-      $("#java").show()
-    }
+}
+
     });
   });
